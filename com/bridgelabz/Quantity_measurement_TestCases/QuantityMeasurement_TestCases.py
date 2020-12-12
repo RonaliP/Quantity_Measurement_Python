9526ca0c4a4459032d349dcaefdf5dbe75f57679
Ronali <ronalipanigrahy88@gmail.com>
2020-12-12 10:13:24 +0530

    6 TEST CASES ADDED FOR UC2 FOR COMPARING DIFFERENT LENGTHS

--- com/bridgelabz/Quantity_measurement_TestCases/QuantityMeasurement_TestCases.py
+++ com/bridgelabz/Quantity_measurement_TestCases/QuantityMeasurement_TestCases.py
@@ -1,6 +1,7 @@
 from com.bridgelabz.quantitymeasurement.Feet import Feet
 from com.bridgelabz.quantitymeasurement.Yard import Yard
 from com.bridgelabz.quantitymeasurement.Inch import Inch
+from com.bridgelabz.quantitymeasurement.QuantityMeasurement import QuantityMeasurement, Length
 import pytest
 
 
@@ -41,6 +42,8 @@ def test_givenTwoDifferentFeetValue_WhenCompared_ShouldReturnFalse():
 
 #___________________________________________________________________
 # Test_case for YARD
+
+
 def test_givenTwoYardValue_WhenCompared_ShouldReturnTrue():
     first_yard = Yard(0)
     second_yard = Yard(0)
@@ -72,6 +75,7 @@ def test_givenTwoDifferentYardValue_WhenCompared_ShouldReturnFalse():
 
 #_______________________________________________________________________
 # Test_case for Inch
+
 def test_givenTwoInchValue_WhenCompared_ShouldReturnTrue():
     first_inch = Inch(0)
     second_inch = Inch(0)
@@ -100,3 +104,52 @@ def test_givenTwoDifferentInchValue_WhenCompared_ShouldReturnFalse():
     first_inch = Inch(0.0)
     second_inch = Inch(1.0)
     assert first_inch != second_inch
+
+
+#______________________________________________________________________
+#UC2-
+
+#3ft=1yard
+def test_3FeetAnd_1YardValue_Compared_ReturnTrue():
+    first_feet = QuantityMeasurement(Length.FEET,3.0)
+    second_yard = QuantityMeasurement(Length.YARD,1.0)
+    with pytest.raises(AssertionError):
+        assert first_feet == second_yard
+
+#  1ft != 1yd
+def test_1FeetAnd_1YardValue_Compared_ReturnFalse():
+    first_feet = QuantityMeasurement(Length.FEET,1.0)
+    second_yard = QuantityMeasurement(Length.YARD,1.0)
+    assert first_feet != second_yard
+
+# 1in != 1yd
+def test_OneInch_OneYardValue_Compared_ReturnFalse():
+    first_inch = QuantityMeasurement(Length.INCH,1.0)
+    second_yard = QuantityMeasurement(Length.YARD,1.0)
+    assert first_inch != second_yard
+
+
+# 36in = 1yd
+def test_36InchAnd_OneYardValue_WhenCompared_ReturnTrue():
+    first_inch = QuantityMeasurement(Length.INCH,36.0)
+    second_yard = QuantityMeasurement(Length.YARD,1.0)
+    with pytest.raises(AssertionError):
+        assert first_inch == second_yard
+
+
+# Test_case5 1yd = 3ft
+def test_OneYardAnd_3FeetValue_Compared_ReturnTrue():
+    first_yard = QuantityMeasurement(Length.YARD,1.0)
+    second_feet = QuantityMeasurement(Length.FEET,3.0)
+    with pytest.raises(AssertionError):
+        assert first_yard == second_feet
+
+
+# Test_case 6 1yd = 36in
+def test_OneYardAnd_36InchValue_WhenCompared_ReturnTrue():
+    first_yard = QuantityMeasurement(Length.YARD,1.0)
+    second_inch = QuantityMeasurement(Length.INCH,36.0)
+    with pytest.raises(AssertionError):
+        assert first_yard == second_inch
+
+#___________________________________________________________________________
