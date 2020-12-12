8966def5ffe704f8855000fb5ac24879827899ae
Ronali <ronalipanigrahy88@gmail.com>
2020-12-12 10:11:36 +0530

    made my code generic

---
+++ com/bridgelabz/quantitymeasurement/QuantityMeasurement.py
@@ -0,0 +1,23 @@
+import enum
+class QuantityMeasurement:
+    def __init__(self, unit, length):
+        self.unit = unit
+        self.length = length
+
+    def __eq__(self, other):
+        if isinstance(other, QuantityMeasurement):
+            return self.unit == other.unit and other.length == self.length
+        if isinstance(self.unit, Length) and isinstance(other.unit, Length):
+            if Length.convert(self.unit, self.length) == Length.convert(other.unit, other.length):
+                return True
+        return False
+
+    def convert(self, length):
+        return self.unit * length
+
+class Length(enum.Enum):
+    INCH = 1.0
+    FEET = 12.0
+    YARD = 36.0
+
+
